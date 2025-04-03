fc76f4ffa24b9ce003a3b0d8560e5a9188b3c20f
Trần Phước Anh <anhtp.22it@vku.udn.vn>
2025-04-03 20:13:41 +0700

    complete Problem 509

---
+++ src/p509/Solution.java
@@ -0,0 +1,17 @@
+package p509;
+
+public class Solution {
+    public int fib(int n) {
+        if (n == 0) {
+            return 0;
+        }
+        if (n == 1) {
+            return 1;
+        }
+        return fib(n-1) + fib(n-2);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new Solution().fib(4));
+    }
+}
